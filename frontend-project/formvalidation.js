398fa9287cf2d5faf499c3e7ce99c02d14fe9e1a
VinothikaR <vinothikaramu@gmail.com>
2025-08-11 20:03:05 +0530

    form validation

--- frontend-project/formvalidation.js
+++ frontend-project/formvalidation.js
@@ -14,12 +14,44 @@ document.getElementById("submit").addEventListener("click",function(){
     error.textContent = "";
     success.textContent = "";
     loading.textContent = "";
-    result.textcontent = "";
+    result.textContent = "";
 
 if(firstname === "" || lastname === "" || mobile === "" || email === "" || password === "" || confirmpassword === ""){
     error.textContent = "All fields are required.";
     return;
 }
+
+const emailPattern = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+  if (!email.match(emailPattern)) {
+    error.textContent = "Invalid email format!";
+    return;
+  }
+
+  if (password.length < 6) {
+    error.textContent = "Password must be at least 6 characters!";
+    return;
+  }
+
+  if (password !== confirmpassword) {
+    error.textContent = "Passwords do not match!";
+    return;
+  }
+
+  // If all validations pass
+  error.textContent = "";
+  loading.textContent = "Submitting...";
+
+  setTimeout(() => {
+    loading.textContent = "";
+    success.textContent = "Form submitted successfully!";
+    result.innerHTML = `
+        <strong>Name:</strong> ${firstname}<br>
+        <strong>Name:</strong> ${lastname}<br>
+        <strong>Email:</strong> ${email}`;
+    setTimeout(() => {
+            window.location.href = "registered.html";
+    }, 1000);
+  }, 2000);
 
 
 })
